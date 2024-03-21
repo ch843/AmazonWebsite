c5e359dc6b557d18b631334fc563bb4b39d3b786
Cassidy Hardisty <ch843@byu.edu>
2024-03-21 07:24:47 -0600

    Initial commit

---
+++ AmazonWebsite/Context/BookContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using AmazonWebsite.Migrations;
+using Microsoft.EntityFrameworkCore;
+
+namespace AmazonWebsite.Context;
+
+public partial class BookContext : DbContext
+{
+    public BookContext()
+    {
+    }
+
+    public BookContext(DbContextOptions<BookContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasIndex(e => e.BookId, "IX_Books_BookID").IsUnique();
+
+            entity.Property(e => e.BookId).HasColumnName("BookID");
+            entity.Property(e => e.Isbn).HasColumnName("ISBN");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
